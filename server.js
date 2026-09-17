051cab8dd8f351ce1e75a18da5d5e555bd2d4161
DLS Student <lutikovluntik97@gmail>
2026-09-17 19:53:46 +0500

    Fix homepage route

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
 // -----------------------------
 // Gemini
 // -----------------------------
